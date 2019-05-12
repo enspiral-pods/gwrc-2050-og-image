bf9fe7e5378cd055880f672416b67f3aa3cebbb0
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2019-05-12 18:15:32 +1200

    enough to set the amount of changein the title, and the direction as a query param

--- src/parser.ts
+++ src/parser.ts
@@ -4,50 +4,54 @@ import { parse } from 'url';
 export function parseRequest(req: IncomingMessage) {
     console.log('HTTP ' + req.url);
     const { pathname = '/', query = {} } = parse(req.url || '', true);
-    const { fontSize, images, widths, heights, theme, md } = query;
+    // const { fontSize, images, widths, heights, theme, md } = query;
+    const increase = (query.increase === 'true');
 
-    if (Array.isArray(fontSize)) {
-        throw new Error('Expected a single fontSize');
-    }
-    if (Array.isArray(theme)) {
-        throw new Error('Expected a single theme');
-    }
-    
+    // if (Array.isArray(fontSize)) {
+    //     throw new Error('Expected a single fontSize');
+    // }
+    // if (Array.isArray(theme)) {
+    //     throw new Error('Expected a single theme');
+    // }
+
     const arr = pathname.slice(1).split('.');
-    let extension = '';
+    // let extension = '';
     let text = '';
+    console.log('arr', arr)
     if (arr.length === 0) {
         text = '';
     } else if (arr.length === 1) {
         text = arr[0];
     } else {
-        extension = arr.pop() as string;
+        // extension = arr.pop() as string;
+        arr.pop()
         text = arr.join('.');
     }
 
-    const parsedRequest: ParsedRequest = {
-        fileType: extension === 'jpeg' ? extension : 'png',
-        text: decodeURIComponent(text),
-        theme: theme === 'dark' ? 'dark' : 'light',
-        md: md === '1' || md === 'true',
-        fontSize: fontSize || '96px',
-        images: getArray(images),
-        widths: getArray(widths),
-        heights: getArray(heights),
+    const parsedRequest: any = {
+        fileType: 'png',
+        text: text,
+        increase: increase
+        // theme: theme === 'dark' ? 'dark' : 'light',
+        // md: md === '1' || md === 'true',
+        // fontSize: fontSize || '96px',
+        // images: getArray(images),
+        // widths: getArray(widths),
+        // heights: getArray(heights),
     };
-    parsedRequest.images = getDefaultImages(parsedRequest.images, parsedRequest.theme);
+    // parsedRequest.images = getDefaultImages(parsedRequest.images, parsedRequest.theme);
     return parsedRequest;
 }
 
-function getArray(stringOrArray: string[] | string): string[] {
-    return Array.isArray(stringOrArray) ? stringOrArray : [stringOrArray];
-}
+// function getArray(stringOrArray: string[] | string): string[] {
+//     return Array.isArray(stringOrArray) ? stringOrArray : [stringOrArray];
+// }
 
-function getDefaultImages(images: string[], theme: Theme): string[] {
-    if (images.length > 0 && images[0] && images[0].startsWith('https://assets.zeit.co/image/upload/front/assets/design/')) {
-        return images;
-    }
-    return theme === 'light'
-    ? ['https://assets.zeit.co/image/upload/front/assets/design/now-black.svg']
-    : ['https://assets.zeit.co/image/upload/front/assets/design/now-white.svg'];
-}
+// function getDefaultImages(images: string[], theme: Theme): string[] {
+//     if (images.length > 0 && images[0] && images[0].startsWith('https://assets.zeit.co/image/upload/front/assets/design/')) {
+//         return images;
+//     }
+//     return theme === 'light'
+//     ? ['https://assets.zeit.co/image/upload/front/assets/design/now-black.svg']
+//     : ['https://assets.zeit.co/image/upload/front/assets/design/now-white.svg'];
+// }
--- src/template.ts
+++ src/template.ts
@@ -2,30 +2,15 @@
 import { readFileSync } from 'fs';
 import { sanitizeHtml } from './sanitizer';
 
-const regular = readFileSync(`${__dirname}/../.fonts/Inter-Regular.woff2`).toString('base64');
 const bold = readFileSync(`${__dirname}/../.fonts/Inter-Bold.woff2`).toString('base64');
-const mono = readFileSync(`${__dirname}/../.fonts/Vera-Mono.woff2`).toString('base64');
-
 const globe = readFileSync(`${__dirname}/../public/globe.png`).toString('base64');
 
-function getCss(theme: string, fontSize: string) {
-    let background = 'white';
-    let foreground = 'black';
-    let radial = 'lightgray';
+function getCss() {
+    const background = 'black';
+    const foreground = 'white';
+    const radial = 'dimgray';
 
-    if (theme === 'dark') {
-        background = 'black';
-        foreground = 'white';
-        radial = 'dimgray';
-    }
     return `
-    @font-face {
-        font-family: 'Inter';
-        font-style:  normal;
-        font-weight: normal;
-        src: url(data:font/woff2;charset=utf-8;base64,${regular}) format('woff2');
-    }
-
     @font-face {
         font-family: 'Inter';
         font-style:  normal;
@@ -33,30 +18,12 @@ function getCss(theme: string, fontSize: string) {
         src: url(data:font/woff2;charset=utf-8;base64,${bold}) format('woff2');
     }
 
-    @font-face {
-        font-family: 'Vera';
-        font-style: normal;
-        font-weight: normal;
-        src: url(data:font/woff2;charset=utf-8;base64,${mono})  format("woff2");
-      }
-
     body {
         background: ${background};
         background-image: radial-gradient(${radial} 5%, transparent 0);
         background-size: 60px 60px;
         height: 100vh;
         display: flex;
-    }
-
-    code {
-        color: #D400FF;
-        font-family: 'Vera';
-        white-space: pre-wrap;
-        letter-spacing: -5px;
-    }
-
-    code:before, code:after {
-        content: '\`';
     }
 
     .logo-wrapper {
@@ -69,12 +36,6 @@ function getCss(theme: string, fontSize: string) {
 
     .logo {
         margin: 75px 0;
-    }
-
-    .plus {
-        color: #BBB;
-        font-family: Times New Roman, Verdana;
-        font-size: 100px;
     }
 
     .spacer {
@@ -91,7 +52,7 @@ function getCss(theme: string, fontSize: string) {
 
     .heading {
         font-family: 'Inter', sans-serif;
-        font-size: ${sanitizeHtml(fontSize)};
+        font-size: 90px;
         font-style: normal;
         font-weight: bold;
         color: ${foreground};
@@ -99,41 +60,27 @@ function getCss(theme: string, fontSize: string) {
     }`;
 }
 
-export function getHtml(parsedReq: ParsedRequest) {
-    const { text, theme } = parsedReq;
+export function getHtml(parsedReq: any) {
+    const { text, increase } = parsedReq;
     return `<!DOCTYPE html>
 <html>
     <meta charset="utf-8">
     <title>Generated Image</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
-        ${getCss(theme, '90px')}
+        ${getCss()}
     </style>
     <body>
         <div class="container">
             <div class="logo-wrapper">
                 <img class="logo" alt="globe" width=174" height=174" src="data:image/png;base64,${globe}" />
             </div>
-            <div class="heading">My choices could reduce</div>
+            <div class="heading">My choices could ${increase ? 'increase' : 'reduce'}</div>
             <div class="heading">Wellington's emissions by</div>
-            <div class="heading">${sanitizeHtml(text)}.</div>
+            <div class="heading">${sanitizeHtml(text)}%.</div>
             <div class="spacer">
             <div class="heading">What would your choices do?</div>
         </div>
     </body>
 </html>`;
 }
-//
-// function getImage(src: string, width ='auto', height = '225') {
-//     return `<img
-//         class="logo"
-//         alt="Generated Image"
-//         src="${sanitizeHtml(src)}"
-//         width="${sanitizeHtml(width)}"
-//         height="${sanitizeHtml(height)}"
-//     />`
-// }
-//
-// function getPlusSign(i: number) {
-//     return i === 0 ? '' : '<div class="plus">+</div>';
-// }
